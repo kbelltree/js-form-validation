2710b8220a0ca0931ffd5486d50f717a1ceb8329
KBelltree <98202109+kbelltree@users.noreply.github.com>
2024-05-11 17:31:12 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,133 @@
+const errorMessages = {
+  email: {
+    required: "Please enter a valid e-mail address.",
+    type: "Valid format is 'example@email.com'",
+    pattern:
+      "Please enter a valid email address in the format 'example@email.com'.",
+  },
+
+  country: {
+    required: "Please select a country.",
+  },
+
+  zip: {
+    required: "Please enter a valid zip-code.",
+    pattern:
+      "Zip code must be up to 10 alphanumeric characters and may include spaces and/or a hyphen.",
+  },
+
+  password1: {
+    required: "Please set up a password between 8 and 20 characters.",
+    pattern:
+      "Password must be 8-20 characters long and can include uppercase, lowercase letters, numbers, and special characters (#?!@$%^&*-).",
+    length: "Please enter within 8 to 20 characters.",
+  },
+
+  password2: {
+    required: "Please re-enter your password for confirmation.",
+    mismatch: "Passwords do not match. Please try again.",
+  },
+};
+
+function getMatchingErrorMessages(controlEl, messageObj) {
+  const errorMessageArr = [];
+  if (controlEl.validity.valueMissing) {
+    errorMessageArr.push(messageObj.required);
+  }
+  if (controlEl.validity.typeMismatch) {
+    errorMessageArr.push(messageObj.type);
+  }
+  if (controlEl.validity.patternMismatch) {
+    errorMessageArr.push(messageObj.pattern);
+  }
+  if (controlEl.validity.tooShort || controlEl.validity.tooLong) {
+    errorMessageArr.push(messageObj.length);
+  }
+  return errorMessageArr;
+}
+
+function accessErrorMessageDiv(controlElId) {
+  if (!controlElId) {
+    console.warn(`controlElId can not be found: ${controlElId}.`);
+    return;
+  }
+  return document.querySelector(`#${controlElId} + .error-message`);
+}
+
+function emptyErrorMessageDiv(divEl) {
+  while (divEl.firstChild) {
+    divEl.removeChild(divEl.firstChild);
+  }
+}
+
+function displayErrorMessage(errorMessageDiv, messageArr) {
+  if (messageArr.length === 0) {
+    console.warn(`there are not any error messages to display.`);
+    return;
+  } else {
+    errorMessageDiv;
+    messageArr.forEach((message) => {
+      const para = document.createElement("p");
+      para.textContent = message;
+      errorMessageDiv.appendChild(para);
+    });
+  }
+  return errorMessageDiv;
+}
+
+function notifyInvalidEntry(controlEl) {
+  const controlElId = controlEl.id;
+  const errorMessageDiv = accessErrorMessageDiv(controlElId);
+  const messagesArr = getMatchingErrorMessages(
+    controlEl,
+    errorMessages[controlElId],
+  );
+
+  emptyErrorMessageDiv(errorMessageDiv);
+  displayErrorMessage(errorMessageDiv, messagesArr);
+  controlEl.classList.remove("is-valid");
+}
+
+function validateEntry(e) {
+  e.preventDefault();
+  const controlEl = e.target;
+  if (!controlEl.checkValidity()) {
+    notifyInvalidEntry(controlEl);
+  } else {
+    controlEl.classList.add("is-valid");
+  }
+}
+
+function validateForm(e) {
+  const form = e.target;
+  const formControls = form.querySelectorAll("input, select");
+  if (!form.checkValidity()) {
+    formControls.forEach((controlEl) => notifyInvalidEntry(controlEl));
+    console.warn("submit cancelled.");
+    e.preventDefault();
+  }
+}
+
+function refreshForm(formEl){
+  errorMessageDivs.forEach((div) => emptyErrorMessageDiv(div));
+  formEl.reset();
+}
+
+const errorMessageDivs = document.querySelectorAll(".error-message");
+const formControls = document.querySelectorAll("input, select");
+const form = document.querySelector("form");
+const resetButton = document.querySelector("#form-reset");
+
+refreshForm(form);
+
+formControls.forEach((controlEl) => {
+  controlEl.addEventListener("blur", validateEntry);
+  controlEl.addEventListener("input", () => {
+    const errorMessageDiv = accessErrorMessageDiv(controlEl.id);
+    emptyErrorMessageDiv(errorMessageDiv);
+  });
+});
+form.addEventListener("submit", validateForm);
+resetButton.addEventListener("click", () => {
+  refreshForm(form);
+})
